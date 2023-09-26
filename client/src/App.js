9212bce90efc76dcae4d58d819fdeef7f9a651c9
Ryan <salvat36@gmail.com>
2023-09-25 20:53:18 -0400

    add tailwindcss and basic front end structure

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-slate-900 underline font-extrabold">Home</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Nav.js
@@ -0,0 +1,6 @@
+
+export const Nav = () => {
+  return (
+    <div>Nav</div>
+  )
+}

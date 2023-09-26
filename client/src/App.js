6bf8b31e12fb9fc80f7e141cfa2a21f8392e5742
Ryan <salvat36@gmail.com>
2023-09-25 21:46:28 -0400

    add and test react-router-dom routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,27 @@
+import { Link } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App bg-slate-200 p-8 flex m-5 justify-end space-x-6">
+      <Link to='/'>
       <h1 className="text-4xl text-pink-900 underline font-extrabold hover:text-blue-600 font-display">
         Home
       </h1>
-      <div class="bg-gray-300">Gray Background</div>
-      <span class="text-red-500">Red Text</span>
-      <button class="bg-green-700 text-white">Green Button</button>
+      </Link>
+      <div>
+      <Link className="text-4xl text-pink-900 underline font-extrabold hover:text-blue-600 font-display" to="courses">Courses</Link>
+      </div>
+      <div>
+      <Link className="text-4xl text-pink-900 underline font-extrabold hover:text-blue-600 font-display" to="contact">Contact</Link>
+      </div>
+      <div>
+      <Link className="text-4xl text-pink-900 underline font-extrabold hover:text-blue-600 font-display" to="login">Login</Link>
+      </div>
+      <div>
+      <Link className="text-4xl text-pink-900 underline font-extrabold hover:text-blue-600 font-display" to="about">About Us</Link>
+      </div>
+      <button class="bg-red-700 text-white hover:bg-blue-600 rounded-xl p-2">Logout</button>
     </div>
   );
 }

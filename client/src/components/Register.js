cc6417054bb9121bcf93e5248386204177a24bb6
Ryan <salvat36@gmail.com>
2023-09-27 00:52:54 -0400

    finish register page formik

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -40,7 +40,74 @@ const Register = () => {
       }
     },
   });
-  return <div>Register</div>;
+
+  if (user) {
+    return null
+  }
+  return <div>
+  <div>
+    <h4>
+      Please Login or Signup!
+    </h4>
+
+    <form onSubmit={formik.handleSubmit} width="25%">
+      <TextField
+        label="Username"
+        variant="outlined"
+        fullWidth
+        margin="normal"
+        name="username"
+        value={formik.values.username}
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        error={formik.touched.username && Boolean(formik.errors.username)}
+        helperText={formik.touched.username && formik.errors.username}
+      />
+      <TextField
+        label="Password"
+        variant="outlined"
+        fullWidth
+        margin="normal"
+        name="password"
+        type="password"
+        value={formik.values.password}
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        error={formik.touched.password && Boolean(formik.errors.password)}
+        helperText={formik.touched.password && formik.errors.password}
+      />
+
+      {!isLoggedIn && (
+        <TextField
+          label="Email"
+          variant="outlined"
+          fullWidth
+          margin="normal"
+          name="email"
+          type="email"
+          value={formik.values.email}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          error={formik.touched.email && Boolean(formik.errors.email)}
+          helperText={formik.touched.email && formik.errors.email}
+        />
+      )}
+      <Button 
+        variant="contained" 
+        color="neutral" 
+        type="submit"
+        sx={{ mt: 2}}
+      >
+          {isLoggedIn ? "Login" : "Create"}{" "}
+        </Button>
+    </form>
+    <FormControlLabel
+      control={<Switch checked={isLoggedIn} onChange={handleLogin} />}
+      label={isLoggedIn ? "Need a new account?" : "Already have an account?"}
+    />
+  </div>
+  {error ? <Error /> : <></>}
+</div>
 };
 
 export default Register;

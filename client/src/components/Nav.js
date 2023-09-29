d6cecf258078aa6f5a03986bda542cdb20fd96f5
Ryan <salvat36@gmail.com>
2023-09-28 22:57:43 -0400

    cleanup links tailwind styles

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -4,40 +4,39 @@ import { UserContext } from "../context/UserProvider";
 
 function Nav() {
   const { handleLogoutClick, user } = useContext(UserContext);
+  const links = "text-4xl text-slate-900 font-extrabold hover:text-blue-600 hover:underline hover:bg-slate-100 font-display border border-slate-900 px-4 py-2 rounded"
 
   return (
     <div className="">
     <div className="p-8 flex m-5 justify-end space-x-4">
       <Link
         to="/"
-        className="text-4xl text-slate-900 font-extrabold hover:text-blue-600 font-display"
+        className="text-4xl text-slate-900 font-extrabold hover:text-blue-600 hover:underline font-display"
       >
         Learnability Pathways
       </Link>
       <Link
-        className="text-4xl text-slate-900 font-extrabold hover:text-blue-600 hover:bg-slate-100 font-display border border-slate-900 px-4 py-2 rounded"
+        className={links}
         to="/"
       >
         Home
       </Link>
         <Link
-          className="text-4xl text-slate-900 font-extrabold hover:text-blue-600  hover:bg-slate-100 font-display border border-slate-900 px-4 py-2 rounded"
+          className={links}
           to="/courses"
         >
           Courses
         </Link>
         {user && (
           <Link
-            className="text-4xl text-slate-900 font-extrabold hover:text-blue-600  hover:bg-slate-100 font-display border border-slate-900 px-4 py-2 rounded"
-            to="/contact"
+            className={links}
           >
             Library
           </Link>
         )}
         {!user && (
           <Link
-            className="text-4xl text-slate-900 font-extrabold hover:text-blue-600  hover:bg-slate-100 font-display border border-slate-900 px-4 py-2 rounded"
-            to="/register"
+            className={links}
           >
             Login
           </Link>
@@ -46,7 +45,7 @@ function Nav() {
         <Link >
         <button
           onClick={handleLogoutClick}
-          className="bg-red-700 text-4xl text-slate-900 font-extrabold hover:text-blue-600  hover:bg-white font-display border border-slate-900 px-4 py-2 rounded"
+          className="bg-red-700 text-4xl text-slate-900 font-extrabold hover:text-blue-600 hover:underline  hover:bg-white font-display border border-slate-900 px-4 py-2 rounded"
         >
           Logout
         </button>

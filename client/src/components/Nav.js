6294d5f050a59c234066a7a4126f0639eb4f6384
Ryan <salvat36@gmail.com>
2023-09-28 20:49:15 -0400

    change route visibility based on user/non-user

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,57 +1,58 @@
-import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
-import { UserContext } from '../context/UserProvider';
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { UserContext } from "../context/UserProvider";
 
 function Nav() {
-
-  const { handleLogoutClick, user} = useContext(UserContext);
+  const { handleLogoutClick, user } = useContext(UserContext);
 
   return (
-    <div className="p-8 flex m-5 justify-between">
-            <Link to="/">
-        <h1 className="text-4xl text-slate-900 font-extrabold hover:text-blue-600 font-display">
+    <div className="">
+    <div className="p-8 flex m-5 justify-end space-x-4">
+      <Link
+        to="/"
+        className="text-4xl text-slate-900 font-extrabold hover:text-blue-600 font-display"
+      >
         Learnability Pathways
-        </h1>
       </Link>
-      <Link to="/">
-        <h1 className="text-4xl text-slate-900 font-extrabold hover:text-blue-600 font-display border border-slate-900 px-4 py-2 rounded">
-          Home
-        </h1>
+      <Link
+        className="text-4xl text-slate-900 font-extrabold hover:text-blue-600 hover:bg-slate-100 font-display border border-slate-900 px-4 py-2 rounded"
+        to="/"
+      >
+        Home
       </Link>
-      <div>
         <Link
-          className="text-4xl text-slate-900 font-extrabold hover:text-blue-600 font-display border border-slate-900 px-4 py-2 rounded"
-          to="/courses" 
+          className="text-4xl text-slate-900 font-extrabold hover:text-blue-600  hover:bg-slate-100 font-display border border-slate-900 px-4 py-2 rounded"
+          to="/courses"
         >
           Courses
         </Link>
-      </div>
-      <div>
-        <Link
-          className="text-4xl text-slate-900 font-extrabold hover:text-blue-600 font-display border border-slate-900 px-4 py-2 rounded"
-          to="/contact" 
+        {user && (
+          <Link
+            className="text-4xl text-slate-900 font-extrabold hover:text-blue-600  hover:bg-slate-100 font-display border border-slate-900 px-4 py-2 rounded"
+            to="/contact"
+          >
+            Library
+          </Link>
+        )}
+        {!user && (
+          <Link
+            className="text-4xl text-slate-900 font-extrabold hover:text-blue-600  hover:bg-slate-100 font-display border border-slate-900 px-4 py-2 rounded"
+            to="/register"
+          >
+            Login
+          </Link>
+        )}
+      {user && (
+        <button
+          onClick={handleLogoutClick}
+          className="bg-red-700 text-white hover:bg-blue-600 rounded-xl p-2 font-display"
         >
-          Library
-        </Link>
-      </div>
-      <div>
-      { !user && (  <Link
-          className="text-4xl text-slate-900 font-extrabold hover:text-blue-600 font-display border border-slate-900 px-4 py-2 rounded"
-          to="/register" 
-        >
-          Login
-        </Link>)}
-      </div>
-     {user && ( <button onClick={handleLogoutClick}className="bg-red-700 text-white hover:bg-blue-600 rounded-xl p-2 font-display">
-        Logout
-      </button>)}
+          Logout
+        </button>
+      )}
+    </div>
     </div>
   );
 }
 
 export default Nav;
-
-
-
-
-

5a7093c2f6a78ed7735ea62595435704711e17e5
Ryan <salvat36@gmail.com>
2023-09-30 15:19:39 -0400

    correct issue with course images being skewed

--- client/src/components/Courses.js
+++ client/src/components/Courses.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext} from "react";
+import React, { useState, useContext } from "react";
 import { ContentContext } from "../context/ContentProvider";
 import ImageList from "@mui/material/ImageList";
 import ImageListItem from "@mui/material/ImageListItem";
@@ -23,48 +23,71 @@ export default function CourseCatalog() {
     handleCourse(filteredCourses);
   };
 
+  const numColumns = course.length >= 4 ? 2 : 1;
+  const itemWidth = 950 / numColumns;
+
   return (
     <>
-    <div style={{ display: "flex", alignItems: "center" }}>
-      <div>
-      <h1 className="font-display text-4xl mb-5 mt-20 ml-25 mx-20">Course Catalog</h1>
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <div>
+          <h1 className="font-display text-4xl mb-5 mt-20 ml-25 mx-20">
+            Course Catalog
+          </h1>
+        </div>
+        <div
+          className="mb-5 mt-20 ml-25 mx-20"
+          style={{ marginLeft: "auto" }}
+        >
+          <Box sx={{ minWidth: 120 }}>
+            <FormControl sx={{ minWidth: 250 }}>
+              <InputLabel id="demo-simple-select-label">Difficulty</InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={difficulty}
+                label="Difficulty"
+                onChange={handleChange}
+              >
+                <MenuItem value="All">All</MenuItem>
+                <MenuItem value="Beginner">Beginner</MenuItem>
+                <MenuItem value="Intermediate">Intermediate</MenuItem>
+                <MenuItem value="Expert">Expert</MenuItem>
+              </Select>
+            </FormControl>
+          </Box>
+        </div>
       </div>
-      <div className="mb-5 mt-20 ml-25 mx-20" style={{ marginLeft: "auto" }}>
-      <Box sx={{ minWidth: 120 }}>
-        <FormControl sx={{ minWidth: 250 }}>
-          <InputLabel id="demo-simple-select-label">Difficulty</InputLabel>
-          <Select
-            labelId="demo-simple-select-label"
-            id="demo-simple-select"
-            value={difficulty}
-            label="Difficulty"
-            onChange={handleChange}
-          >
-            <MenuItem value="All">All</MenuItem>
-            <MenuItem value="Beginner">Beginner</MenuItem>
-            <MenuItem value="Intermediate">Intermediate</MenuItem>
-            <MenuItem value="Expert">Expert</MenuItem>
-          </Select>
-        </FormControl>
-      </Box>
-      </div>
-      </div>
-      <div style={{ display: "flex", justifyContent: "center" }}>
-        <ImageList sx={{ width: 1000, height: 950 }}>
+      <div style={{ display: "flex", justifyContent: "center"}}>
+        <ImageList
+          sx={{
+            width: 1000,
+            height: "auto",
+            flexWrap: "wrap",
+            columns: numColumns,
+            overflow: 'hidden'
+          }}
+        >
           {course
             .filter(
               (item) => difficulty === "All" || item.difficulty === difficulty
             )
             .map((item) => (
+              <div>
               <ImageListItem
                 key={item.course_image}
-                sx={{ marginX: 2, width: 450, height: 200 }}
+                sx={{
+                  marginX: 2,
+                  width: `${itemWidth}px`,
+                  height: "auto",
+                }}
               >
                 <img
                   srcSet={`${item.course_image}?w=248&fit=crop&auto=format&dpr=2 2x`}
                   src={`${item.course_image}?w=248&fit=crop&auto=format`}
                   alt={item.title}
                   loading="lazy"
+                  style={{ objectFit: "cover", width: "100%", height: "100%" }}
+                  className="hover:animate-pulse"
                 />
                 <ImageListItemBar
                   sx={{ marginY: 5 }}
@@ -77,9 +100,10 @@ export default function CourseCatalog() {
                   position="below"
                 />
               </ImageListItem>
+              </div>
             ))}
         </ImageList>
       </div>
-      </>
+    </>
   );
 }
--- client/src/components/Filter.js
+++ client/src/components/Filter.js
@@ -1,41 +1,41 @@
-import Box from "@mui/material/Box";
-import InputLabel from "@mui/material/InputLabel";
-import MenuItem from "@mui/material/MenuItem";
-import FormControl from "@mui/material/FormControl";
-import Select from "@mui/material/Select";
-import { useState, useContext } from "react";
-import { ContentContext } from "../context/ContentProvider";
-import { ErrorContext } from "../context/ErrorProvider";
+// import Box from "@mui/material/Box";
+// import InputLabel from "@mui/material/InputLabel";
+// import MenuItem from "@mui/material/MenuItem";
+// import FormControl from "@mui/material/FormControl";
+// import Select from "@mui/material/Select";
+// import { useState, useContext } from "react";
+// import { ContentContext } from "../context/ContentProvider";
+// import { ErrorContext } from "../context/ErrorProvider";
 
-export default function Filter() {
-  const [difficulty, setDifficulty] = useState("");
-  const { course, handleCourse } = useContext(ContentContext);
+// export default function Filter() {
+//   const [difficulty, setDifficulty] = useState("");
+//   const { course, handleCourse } = useContext(ContentContext);
 
-  const handleChange = (event) => {
-    const selectedDifficulty = event.target.value;
-    setDifficulty(selectedDifficulty);
-    const filteredCourses = course.filter(
-      (course) => course.difficulty === selectedDifficulty
-    );
-    handleCourse(filteredCourses);
-  };
+//   const handleChange = (event) => {
+//     const selectedDifficulty = event.target.value;
+//     setDifficulty(selectedDifficulty);
+//     const filteredCourses = course.filter(
+//       (course) => course.difficulty === selectedDifficulty
+//     );
+//     handleCourse(filteredCourses);
+//   };
 
-  return (
-    <Box sx={{ minWidth: 120 }}>
-      <FormControl sx={{ minWidth: 250 }}>
-        <InputLabel id="demo-simple-select-label">Difficulty</InputLabel>
-        <Select
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          value={difficulty}
-          label="Difficulty"
-          onChange={handleChange}
-        >
-          <MenuItem value='Beginner'>Beginner</MenuItem>
-          <MenuItem value='Intermediate'>Intermediate</MenuItem>
-          <MenuItem value='Expert'>Expert</MenuItem>
-        </Select>
-      </FormControl>
-    </Box>
-  );
-}
+//   return (
+//     <Box sx={{ minWidth: 120 }}>
+//       <FormControl sx={{ minWidth: 250 }}>
+//         <InputLabel id="demo-simple-select-label">Difficulty</InputLabel>
+//         <Select
+//           labelId="demo-simple-select-label"
+//           id="demo-simple-select"
+//           value={difficulty}
+//           label="Difficulty"
+//           onChange={handleChange}
+//         >
+//           <MenuItem value='Beginner'>Beginner</MenuItem>
+//           <MenuItem value='Intermediate'>Intermediate</MenuItem>
+//           <MenuItem value='Expert'>Expert</MenuItem>
+//         </Select>
+//       </FormControl>
+//     </Box>
+//   );
+// }

e774856a1eb204a266dbb22d8eeef26d828e59ad
Ryan <salvat36@gmail.com>
2023-10-01 10:08:54 -0400

    final touch

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 function Footer() {
   return (
-    <footer className="bg-[#ffffff] text-black">
+    <footer className="bg-[#ffffff] text-black items-center">
       <div className="bg-slate-900 h-20">
       </div>
-      <div className="container py-6 px-6">
-        <div className="flex justify-between gap-20 align-middle">
+      <div className="container py-6 flex justify-center">
+        <div className="flex gap-x-60 ml-60 mx-auto">
           <div className="footer-section">
             <a href="/terms-of-service">Terms of Service</a>
           </div>

2b053d2446e1b871e87f35d9c53672e960435c1a
JSONRete <jsonrete@gmail.com>
2023-10-04 02:11:15 -0400

    added 'Loading' text to Loading component

--- client/src/components/Loading.js
+++ client/src/components/Loading.js
@@ -1,15 +1,30 @@
 import React from 'react'
 import CircularProgress from '@mui/joy/CircularProgress'
+import Typography from '@mui/material/Typography';
+
+const containerStyle = {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+  minHeight: '100vh', // Ensures the content is vertically centered on the page
+};
 
 const Loading = () => {
   return (
+
+  <div style={containerStyle}>
     <CircularProgress
-  color="neutral"
-  determinate={false}
-  size="lg"
-  value={25}
-  variant="solid"
-/>
+    color="neutral"
+    determinate={false}
+    size="lg"
+    value={25}
+    variant="solid"
+    />
+    <Typography variant="body1" color="textSecondary">
+    Loading...
+    </Typography>
+</div>
   )
 }
 
